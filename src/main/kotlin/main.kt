7a65c174690f11878e077ded88df60c4d8979721
Paul Dailly <paul.dailly@phorest.com>
2021-02-14 21:31:39 +0000

    Added sample notes about high level domain rules/behaviours

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Fruit Machine
+    // Game Session
+    // Player
+    // Player has money
+    // Machine has slots
+    // Machine has money
+
+    // Player starts with a balance
+    // Adds money to machine
+    // Pulls lever on machine
+    // Machine displays result
+    // Machine keeps track of how much it owes
+    // Player can cash out
+
+}
---
+++ src/test/kotlin/FruitMachineTest.kt
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class FruitMachineTest {
+    @Test
+    fun `sample test`() {
+        assertEquals(4, 1 + 3)
+    }
+}
